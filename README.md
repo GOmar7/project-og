2c5609e8d7b2a30b839137e09d39ba5d6b6da6a2
GOmar7 <omargarcia.leon@gmail.com>
2020-05-20 13:21:51 -0400

    first commit

--- README.md
+++ README.md
@@ -3,3 +3,14 @@ Date and time, fecha y hora
 Tue May 19 20:39:27 -04 2020
 Number of lines of  code : 
 27 guessinggame.sh
+# Project: my final  work A Guessing game
+<br>
+<br>
+##**- Date and time, fecha y hora**
+&nbsp
+Wed May 20 13:21:04 -04 2020
+<br>
+<br>
+##**- Number of lines of  code** : 
+&nbsp
+29 guessinggame.sh
--- guessinggame.sh
+++ guessinggame.sh
@@ -1,27 +1,29 @@
 #!/usr/bin/env bash
 
-function suma()
-{
- if [ "$texto" > "$var" ]; then
-echo " tiene que ser menor"
-elif [ "$texto" < "$var" ]; then
-echo "tiene que ser mayor"
-return $suma
-fi
+function variable {
+	v variable_a=$(find . ! -name . -prune -print | grep -c/)
+	echo "El total es: "$variable_a
+	return $variable_a
 }
 
-        echo "Indique cuantos archivos hay | indicate how many files are there"
+        echo "Indique cuantos archivos hay | indicate how many files are there "
         read "texto"
-        var=$(ls -al | wc -l)
 
-while [[ "$texto" != "$var" ]]
+var=$(find . ! -name . -prune -print | grep -c /)
+
+while [[ $texto != "$var" ]]
         do
-echo "Error intente nuevamente | Error, please try again "suma
+			if [ $texto -ge $var ]; then
+			echo " Too high"
+			else
+			if [ $texto -le  $var ]; then
+			echo "Too low"
+		fi
+		fi
+echo "Error intente nuevamente | Error, please try again "
 read "texto"
+
 done
 
-if [ "$texto" = "$var" ]; then
-echo "Respuesta correcta, hay un total de "$var "ficheros"
-echo "Correct answer, congratulations! there are "$var "files in this directory".
-
-fi
+echo "That's correct, congratulations" 
+echo "Total files in this directory  "$variable
